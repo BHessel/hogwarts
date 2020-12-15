6a5194bfb10d84bc95942b52d4758d4440ec30a6
BHessel <hesselfitness@gmail.com>
2020-12-15 11:34:22 -0500

    switch

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
 
+
 export default class Card extends Component {
     render() {
         return (
-            <div>
-                
+            <div className='card'>
+                <div classname='nameHeader'>
+                    <a className='name'>
+                        Ben and Anne!
+                    </a>
+                    <img src= alt="Pig Image" />
+
+                </div>
             </div>
         )
     }
 }
+
+
+  
